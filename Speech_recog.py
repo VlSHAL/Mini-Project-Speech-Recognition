69e371eaace789af11b945607c83c90236dde8cc
Vishal Sharma <36329746+VlSHAL@users.noreply.github.com>
2018-08-15 16:22:42 +0530

    Update 1.0
    Syntax correction

--- Speech_recog.py
+++ Speech_recog.py
@@ -1,11 +1,11 @@
 import speech_recognition as sr
 
 
-recognizer = se.Recognizer()
-audiofile = sr.AudioFile('')
+recognizer = sr.Recognizer()
+audiofile = sr.AudioFile('welcome_to_rubiks_code_dot_net.wav')
 
 with audiofile as source:
     recognizer.adjust_for_ambient_noise(source)
     audio = recognizer.record(source)
 
-recognizer.recognize_google(audio)
+print(recognizer.recognize_google(audio))
